dc44d505214710f9fd53315d107f096c20b76336
Miguel Montoya <miguel.mzbi@gmail.com>
2018-01-22 20:53:28 -0600

    Practica 2 TCL Testing and Languages

--- Practicas/Practica2/main.c
+++ Practicas/Practica2/main.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 #include "operations.h"
+#include "messages.h"
 
 #define MAX_STRING_LENGTH 100
+#define LANG "en"
+
+typedef struct {
+    char *operationName;
+    int(* operation) (int, int);
+} Operation;
 
 int main() {
     char operation[MAX_STRING_LENGTH];
     int operatorOne, operatorTwo;
-    char *operationMessage = "What is the operation?";
+    const char **messages;
 
-    printf("%s\n", operationMessage);
+    if(!strcmp(LANG, "en")) {
+        messages = message_ENG;
+    }
+    else if(!strcmp(LANG, "es")) {
+        messages = message_ESP;
+    }
+
+    printf("%s\n", messages[0]);
     scanf("%s", operation);
     scanf("%d %d", &operatorOne, &operatorTwo);
 
     if(!strcmp(operation, "add")) {
-        printf("Result: %d\n", add(operatorOne, operatorTwo));
+        printf("%s: %d\n", messages[2], add(operatorOne, operatorTwo));
     }
     else if(!strcmp(operation, "substract")){
-        printf("Result: %d\n", substract(operatorOne, operatorTwo));
+        printf("%s: %d\n", messages[2], substract(operatorOne, operatorTwo));
     }
     else {
-        printf("%s\n", "Operation not implemented");  
+        printf("%s\n", messages[1]);  
     }
+
     return 0;
 }
---
+++ Practicas/Practica2/messages.h
@@ -0,0 +1,11 @@
+const char *message_ENG[] = {
+    "What is the operation?",
+    "Operation not implemented yet",
+    "Result"
+};
+
+const char *message_ESP[] = {
+    "¿Cual es la operacion?",
+    "La operacion no ha sido implementada todavia",
+    "Resultado"
+};
