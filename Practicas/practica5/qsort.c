10f286b5d59f35afb6fd81a9db80e8423401e133
Miguel Montoya <miguel.mzbi@gmail.com>
2018-02-19 20:17:50 -0600

    Practica 5

---
+++ Practicas/practica5/qsort.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "qsort.h"
+
+static int compareById(const void *element1, const void *element2) {
+    Student *student1 = (Student *) element1;
+    Student *student2 = (Student *) element2;
+    return student1->id - student2->id;
+}
+
+void sortById(Student * class, int numElements) {
+    qsort((void *) class, (size_t) numElements, (size_t) sizeof(Student), compareById);
+}
+
+static int compareByName(const void *element1, const void *element2) {
+    Student *student1 = (Student *) element1;
+    Student *student2 = (Student *) element2;
+    return strcmp(student1->name, student2->name);
+}
+
+void sortByName(Student * class, int numElements) {
+    qsort((void *) class, (size_t) numElements, (size_t) sizeof(Student), compareByName);
+}
